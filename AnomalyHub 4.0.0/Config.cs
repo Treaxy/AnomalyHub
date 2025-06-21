d5b2b97a16373607b0e5026645e2d79f6eaddb7d
Treaxy <146599871+Treaxy@users.noreply.github.com>
2025-06-22 00:02:08 +0300

    Add files via upload

---
+++ AnomalyHub 4.0.0/Config.cs
@@ -0,0 +1,93 @@
+﻿using Exiled.API.Interfaces;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+namespace AnomalyHub
+{
+    public class Config : IConfig
+    {
+        [Description("Plugin is enabled?")]
+        public bool IsEnabled { get; set; } = true;
+        [Description("Debug(console output)?")]
+        public bool Debug { get; set; } = false;
+        [Description("Chaos Insurgency spawn, CASSIE message is enabled?")]
+        public bool CIspawnAnnounce { get; set; } = false;
+        [Description("Chaos Insurgency spawn, CASSIE message")]
+        public string CIspawnMessage { get; set; } = "Chaos Insurgency HasEntered from Gate Alpha";
+        [Description("Chaos Insurgency spawn, CASSIE message sending delay(1 = 1 second)")]
+        public float CIannoDelay { get; set; } = 1;
+        [Description("Chaos Insurgency spawn lights is enabled?")]
+        public bool CIspawnLight { get; set; } = true;
+        [Description("Chaos Insurgency spawn light color. I prefer do not type 10 or higher")]
+        public Dictionary<int, Color> CIspawnLightColor { get; set; } = new Dictionary<int, Color>()
+        {
+            { 1, new Color(0f,1f,0f) },
+        };
+        [Description("Nine Tailed Fox spawn lights is enabled?")]
+        public bool NTFspawnLight { get; set; } = true;
+        [Description("Nine Tailed Fox spawn light color. I prefer do not type 10 or higher")]
+        public Dictionary<int, Color> NTFspawnLightColor { get; set; } = new Dictionary<int, Color>()
+        {
+            { 1, new Color(0f,0f,1f) },
+        };
+        [Description("UIU and SH spawn lights is enabled?(UIU and SerpentsHand plugin connection)")]
+        public bool UIUandSHLight { get; set; } = true;
+        [Description("UIU and SH spawn light color. I prefer do not type 10 or higher")]
+        public Dictionary<int, Color> UIUandSHspawnLightColor { get; set; } = new Dictionary<int, Color>()
+        {
+            { 1, new Color(1f,0f,0f) },
+        };
+        [Description("Round start CASSIE message.")]
+        public string RoundStartMessage { get; set; } = "pitch_0.2 .g4 .g4 pitch_0.9 The Site is Experiencing Many Keter and Euclid Level SCP Containment Breaches. Full Site Lock down Initiated. pitch_1.0";
+        [Description("Round start red light time. Default is 30 second.")]
+        public ushort RoundStartLight { get; set; } = 30;
+        [Description("Round ending AutoFF is enabled?")]
+        public bool AutoFF { get; set; } = true;
+        [Description("AutoFF broadcast message.")]
+        public string AutoFFBroadcast { get; set; } = "FriendlyFire Active.";
+        [Description("AutoFF broadcast duration. Default is 5 second.")]
+        public ushort AutoFFbroadcastDur { get; set; } = 5;
+        [Description("Player join message. Type '%player%' for player name.")]
+        public string EnterBroadcast { get; set; } = "Welcome to our server %player%";
+        [Description("Can other players see join message?")]
+        public bool EnterBroadcastShow { get; set; } = true;
+        [Description("Player join message duration.")]
+        public ushort EnterBroadcastDur { get; set; } = 10;
+        [Description("Granade is allowed?")]
+        public bool AntiBomb { get; set; } = false;
+        [Description("Micro is allowed?")]
+        public bool AntiMicro { get; set; } = false;
+        [Description("Delayed broadcast is enabled?")]
+        public bool AutoBroadcast { get; set; } = true;
+        [Description("Delayed broadcast message")]
+        public string AutoBroadcastME { get; set; } = "join our discord server! discord.gg/discord";
+        [Description("Delayed broadcast delay time. Default is 300 second(5 minutes)")]
+        public float AutoBroadcastDur { get; set; } = 300;
+        [Description("You want to use default HP's in the game?")]
+        public bool DefaultHP { get; set; } = false;
+        [Description("You want to use default AHP's in the game?")]
+        public bool DefaultAHP { get; set; } = true;
+        [Description("SCP-096 new AHP")]
+        public ushort SCP096newAHP { get; set; } = 0;
+        [Description("SCP-106 new AHP")]
+        public ushort SCP106newAHP { get; set; } = 0;
+        [Description("SCP-173 new AHP")]
+        public ushort SCP173newAHP { get; set; } = 0;
+        [Description("SCP-939 new AHP")]
+        public ushort SCP939newAHP { get; set; } = 0;
+        [Description("SCP-049 new AHP")]
+        public ushort SCP049newAHP { get; set; } = 0;
+        [Description("SCP-096 new HP")]
+        public ushort SCP096newHP { get; set; } = 2500;
+        [Description("SCP-106 new HP")]
+        public ushort SCP106newHP { get; set; } = 2200;
+        [Description("SCP-173 new HP")]
+        public ushort SCP173newHP { get; set; } = 4000;
+        [Description("SCP-939 new HP")]
+        public ushort SCP939newHP { get; set; } = 2700;
+        [Description("SCP-049 new HP")]
+        public ushort SCP049newHP { get; set; } = 2200;
+
+    }
+}
---
+++ AnomalyHub 4.0.0/EventHandlers.cs
@@ -0,0 +1,267 @@
+﻿using Exiled.Events.EventArgs.Server;
+using System;
+using MEC;
+using UnityEngine;
+using Cassie = Exiled.API.Features.Cassie;
+using Map = Exiled.API.Features.Map;
+using Server = Exiled.API.Features.Server;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using System.Collections.Generic;
+using PlayerRoles;
+
+namespace AnomalyHub
+{
+    public class EventHandlers
+    {
+        bool blue = false;
+        bool red = false;
+        bool gr = false;
+        int count = 0;
+        public void RoundEndEvent(RoundEndedEventArgs ev)
+        {
+            try
+            {
+                if (Plugin.Instance.Config.AutoFF)
+                {
+                    Map.Broadcast(Plugin.Instance.Config.AutoFFbroadcastDur, Plugin.Instance.Config.AutoFFBroadcast);
+                    Server.FriendlyFire = true; Log.Debug("Round Ending, AutoFF is now Active.");
+                }
+                Timing.KillCoroutines(autoBroCoroutine);
+            }
+            catch (Exception e)
+            {
+                Log.Error("RoundEndedEventArgs: " + e.Message.ToString());
+            }
+        }
+
+        public void PlayerEnter(VerifiedEventArgs ev)
+        {
+            if (ev.Player == null)
+            {
+                Log.Debug("Null");
+                return;
+            }
+            if (Plugin.Instance.Config.EnterBroadcastShow)
+            {
+                Map.Broadcast(Plugin.Instance.Config.EnterBroadcastDur, Plugin.Instance.Config.EnterBroadcast.Replace("%player%", ev.Player.DisplayNickname));
+                return;
+            }
+            ev.Player.Broadcast(Plugin.Instance.Config.EnterBroadcastDur, Plugin.Instance.Config.EnterBroadcast.Replace("%player%", ev.Player.DisplayNickname));
+            Log.Debug(ev.Player.DisplayNickname + " Joined this Server, broadcast will be show shortly.");
+        }
+
+        public void PlayerWait()
+        {
+            if (Plugin.Instance.Config.AutoFF)
+            {
+                Server.FriendlyFire = false;
+                Log.Debug("Players Waiting, AutoFF is now Disabled.");
+            }
+        }
+
+        public void AntiItems(ChangingItemEventArgs ev)
+        {
+            try
+            {
+                if (Plugin.Instance.Config.AntiBomb && !(ev.Item == null))
+                {
+                    if (ev.Item.Type == ItemType.GrenadeHE)
+                    {
+                        ev.Item.Destroy(); Log.Debug("Grenade deleted.");
+                    }
+                }
+                if (Plugin.Instance.Config.AntiMicro && !(ev.Item == null))
+                {
+                    if (ev.Item.Type == ItemType.MicroHID)
+                    {
+                        ev.Item.Destroy(); Log.Debug("Micro deleted.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error("ChangingItemEventArgs: " + ex.Message.ToString());
+            }
+        }
+
+        public IEnumerator<float> light(bool ci, bool sh, bool ntf)
+        {
+            while (!Round.IsEnded)
+            {
+                if (count > 4)
+                {
+                    count = 0;
+                    break;
+                }
+                count++;
+                if (ci)
+                {
+                    if (gr)
+                    {
+                        Map.ChangeLightsColor(Color.clear);
+                        gr = false;
+                    }
+                    else
+                    {
+                        Color color = Plugin.Instance.Config.CIspawnLightColor.TryGetValue(1, out Color col) ? col : Color.clear;
+                        Map.ChangeLightsColor(color);
+                        gr = true;
+                        Log.Debug($"Chaos Insurgency Lights: R={color.r}, G={color.g}, B={color.b}, A={color.a}");
+                        if (color.g > 2 || color.r > 2 || color.b > 2)
+                        {
+                            Log.Warn("Chaos Insurgency Spawn Light Brightness level is too high!");
+                        }
+                    }
+                }
+                else if (sh)
+                {
+                    if (red)
+                    {
+                        Map.ChangeLightsColor(Color.clear);
+                        red = false;
+                    }
+                    else
+                    {
+                        Color color = Plugin.Instance.Config.UIUandSHspawnLightColor.TryGetValue(1, out Color col) ? col : Color.clear;
+                        Map.ChangeLightsColor(color);
+                        if (color.r > 10 || color.g > 10 || color.b > 10)
+                        {
+                            Log.Warn("UIU or SH Spawn Light Brightness level is too high!");
+                        }
+                        red = true;
+                    }
+                }
+                if (ntf)
+                {
+                    if (blue)
+                    {
+                        Map.ChangeLightsColor(Color.clear);
+                        blue = false;
+                    }
+                    else
+                    {
+                        Color color = Plugin.Instance.Config.NTFspawnLightColor.TryGetValue(1, out Color col) ? col : Color.clear;
+                        Map.ChangeLightsColor(color);
+                        blue = true;
+                        if (color.g > 2 || color.r > 2 || color.b > 2)
+                        {
+                            Log.Warn("NTF Spawn Light Brightness level is too high!");
+                        }
+                    }
+                }
+                yield return Timing.WaitForSeconds(1f);
+            }
+            Map.ChangeLightsColor(Color.clear);
+        }
+
+        public void Spawn(RespawningTeamEventArgs ev)
+        {
+            try
+            {
+                var ntf = ev.Wave.SpawnableFaction == Exiled.API.Enums.SpawnableFaction.NtfWave;
+                var ntfmini = ev.Wave.SpawnableFaction == Exiled.API.Enums.SpawnableFaction.NtfMiniWave;
+                var chaos = ev.Wave.SpawnableFaction == Exiled.API.Enums.SpawnableFaction.ChaosWave;
+                var chaosmini = ev.Wave.SpawnableFaction == Exiled.API.Enums.SpawnableFaction.ChaosMiniWave;
+                if (ntf || ntfmini)
+                {
+                    if (Plugin.Instance.Config.NTFspawnLight)
+                    {
+                        Timing.RunCoroutine(light(false, false, true)); Log.Debug("NTF spawned.");
+                    }
+                }
+                if (chaos || chaosmini)
+                {
+                    if (Plugin.Instance.Config.CIspawnLight)
+                    {
+                        Timing.RunCoroutine(light(true, false, false)); Log.Debug("Chaos Insurgency spawned.");
+                    }
+                }
+                if (!(ntf || ntfmini || chaos || chaosmini))
+                {
+                    if (Plugin.Instance.Config.UIUandSHLight)
+                    {
+                        Timing.RunCoroutine(light(false, true, false)); Log.Debug("UIU or SH spawned.");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Error("RespawningTeamEventArgs: " + e.Message.ToString());
+            }
+        }
+        public void NewHealths(SpawningEventArgs ev)
+        {
+            try
+            {
+                if (!Plugin.Instance.Config.DefaultHP)
+                {
+                    Dictionary<RoleTypeId, float> RoleHealth = new Dictionary<RoleTypeId, float>()
+                    {
+                        { RoleTypeId.Scp096, Plugin.Instance.Config.SCP096newHP },
+                        { RoleTypeId.Scp106, Plugin.Instance.Config.SCP106newHP },
+                        { RoleTypeId.Scp173, Plugin.Instance.Config.SCP173newHP },
+                        { RoleTypeId.Scp939, Plugin.Instance.Config.SCP939newHP },
+                        { RoleTypeId.Scp049, Plugin.Instance.Config.SCP049newHP }
+                    };
+
+                    if (RoleHealth.ContainsKey(ev.Player.Role))
+                    {
+                        ev.Player.MaxHealth = RoleHealth[ev.Player.Role]; Log.Debug("New SCP healts been set!");
+                    }
+                    
+                }
+                if (!Plugin.Instance.Config.DefaultAHP)
+                {
+                    Dictionary<RoleTypeId, float> RoleAHP = new Dictionary<RoleTypeId, float>()
+                    {
+                        { RoleTypeId.Scp096, Plugin.Instance.Config.SCP096newAHP },
+                        { RoleTypeId.Scp106, Plugin.Instance.Config.SCP106newAHP },
+                        { RoleTypeId.Scp173, Plugin.Instance.Config.SCP173newAHP },
+                        { RoleTypeId.Scp939, Plugin.Instance.Config.SCP939newAHP },
+                        { RoleTypeId.Scp049, Plugin.Instance.Config.SCP049newAHP }
+                    };
+                    if (RoleAHP.ContainsKey(ev.Player.Role))
+                    {
+                        ev.Player.ArtificialHealth = RoleAHP[ev.Player.Role]; Log.Debug("New SCP ahp's been set!");
+                    }
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Error("SpawningEventArgs: " + e.Message.ToString());
+            }
+        }
+
+        CoroutineHandle autoBroCoroutine;
+        public IEnumerator<float> AutoBro()
+        {
+            Map.Broadcast(5, Plugin.Instance.Config.AutoBroadcastME); Log.Debug("AutoBroadcast sent!");
+
+            yield return Timing.WaitForSeconds(Plugin.Instance.Config.AutoBroadcastDur);
+        }
+
+        public void OnRoundStarted()
+        {
+            try
+            {
+                Map.ChangeLightsColor(Color.red); Log.Debug("Round started. Color set to red!");
+                Cassie.Message(Plugin.Instance.Config.RoundStartMessage, true, true, true);
+                Timing.CallDelayed(Plugin.Instance.Config.RoundStartLight, () =>
+                {
+                    Map.ChangeLightsColor(Color.clear); Log.Debug("Red Color Cleared.");
+                });
+                if (Plugin.Instance.Config.AutoBroadcast)
+                {
+                    Timing.RunCoroutine(AutoBro());
+                }
+            }
+
+            catch (Exception e)
+            {
+                Log.Error("OnRoundStarted: " + e.Message.ToString());
+            }
+        }
+    }
+}
